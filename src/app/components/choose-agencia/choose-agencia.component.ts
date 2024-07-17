30f94a12466d5057de2bd7ee189d12c24450ca82
knarfangelo <knarf2003angelo@gmail.com>
2024-07-17 12:18:33 -0500

    Arreglando el borrado de servicio

--- src/app/components/choose-agencia/choose-agencia.component.ts
+++ src/app/components/choose-agencia/choose-agencia.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavegacionComponent } from '../navegacion/navegacion.component';
 import { InicioComponent } from "../inicio/inicio.component";
 import { NosotrosComponent } from "../nosotros/nosotros.component";
-import { ServiciosComponent } from "../servicios/servicios.component";
 
 @Component({
   selector: 'app-choose-agencia',
@@ -12,13 +11,11 @@ import { ServiciosComponent } from "../servicios/servicios.component";
     CommonModule, NavegacionComponent,
     InicioComponent,
     NosotrosComponent,
-    ServiciosComponent
 ],
   template: `
     <app-navegacion></app-navegacion>
     <app-inicio></app-inicio>
     <app-nosotros></app-nosotros>
-    <app-servicios></app-servicios>
   `,
   styleUrls: ['./choose-agencia.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
