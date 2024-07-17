6361d94162388d0704d92493ae2ecd3767bb9b55
knarfangelo <knarf2003angelo@gmail.com>
2024-07-17 14:44:27 -0500

    Prueba del swiper completo

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,16 +1,33 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { register } from 'swiper/element/bundle';
-
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { register, SwiperContainer } from 'swiper/element/bundle';
+import { isPlatformBrowser } from '@angular/common';
+import { SwiperOptions } from 'swiper/types';
+import { serviciosJSON } from './serviciosDB/serviciosJSON';
+import { IServicios } from './serviciosDB/IServicios';
 @Component({
   selector: 'app-servicios',
   standalone: true,
   imports: [
     CommonModule,
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <header>
-        HolaMundoPruebaFinal
+        <swiper-container>
+          @for (item of swiperObjects; track $index) {
+            <swiper-slide>
+              <div class="style-slide">
+                <h2>{{item.title}}</h2>
+                <h4>{{item.subtitle}}</h4>
+                <button>Más info</button>
+                <img [src]="item.img" alt="">
+              </div>
+             
+            </swiper-slide>
+
+          }
+        </swiper-container>
     </header>
   `,
   styleUrl: './servicios.component.css',
@@ -19,17 +36,40 @@ import { register } from 'swiper/element/bundle';
 export class ServiciosComponent implements OnInit {
 
 
+  swiperElements = signal<SwiperContainer | null>(null);
+  swiperObjects: IServicios[] = serviciosJSON;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-   
-  }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      register();
+    register();
+    const swiperElemConstructor = document.querySelector('swiper-container');
+    const swiperOptions: SwiperOptions = {
+      navigation:{
+        enabled:true,
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+      },
+      slidesPerView: 'auto',
+      speed: 3000,
+      loop:true,
+      spaceBetween:50,
+      breakpoints: {
+        0:{
+          slidesPerView:1,
+        },
+        640: {
+          slidesPerView:2,
+        },
+        1024: {
+          slidesPerView:3,
+        },
+      },
+    };
+    Object.assign(swiperElemConstructor!, swiperOptions);
+    this.swiperElements.set(swiperElemConstructor as SwiperContainer);
+    this.swiperElements()?.initialize();
     }
-
-
   }
-  
-  
 }
